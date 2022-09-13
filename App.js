f6d0a088d3d378d17682994dff0ccccdeddfc459
biloholovskyi <51454763+biloholovskyi@users.noreply.github.com>
2022-09-13 22:13:34 +0300

    prettier

--- App.js
+++ App.js
@@ -1,26 +1,19 @@
-import * as React from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-  SafeAreaView,
-  Platform,
-  StatusBar,
-} from 'react-native';
+import * as React from 'react'
+import { StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native'
 
 import { Focus } from './src/features/focus/Focus'
 
-import { colors } from './src/utils/colors';
+import { colors } from './src/utils/colors'
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Focus />
     </SafeAreaView>
-  );
+  )
 }
 
-console.log(StatusBar.currentHeight);
+console.log(StatusBar.currentHeight)
 
 const styles = StyleSheet.create({
   container: {
@@ -28,4 +21,4 @@ const styles = StyleSheet.create({
     padding: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
     backgroundColor: colors.darkBlue,
   },
-});
+})
